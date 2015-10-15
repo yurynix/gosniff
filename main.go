f0bcdba52de498de4b972a083c9292b579dd7ffc
Yury Michurin <yury@nix.co.il>
2015-10-15 16:38:59 +0300

    Ugly hack to read many files and listen on all interfaces

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ package main
 
 import (
 	zmq "github.com/pebbe/zmq4"
+	"path/filepath"
 	"flag"
 	"fmt"
 	"github.com/google/gopacket"
@@ -22,11 +23,14 @@ import (
 )
 
 var queues_num = flag.Int("q", 2, "Num of queues to publish to")
+var decoder     = flag.String("decoder", "Ethernet", "Name of the decoder to use")
 var iface = flag.String("i", "eth0", "Interface to read packets from")
 var fname = flag.String("r", "", "Filename to read from, overrides -i")
 var snaplen = flag.Int("s", 65536, "Snap length (number of bytes max to read per packet")
 var tstype = flag.String("timestamp_type", "", "Type of timestamps to use")
 var promisc = flag.Bool("promisc", true, "Set promiscuous mode")
+var processed_packets = 0
+var publishers []*zmq.Socket = nil
 
 func create_pub_socket(addr string) (*zmq.Socket) {
 	socket, _ := zmq.NewSocket(zmq.PUB)
@@ -38,20 +42,60 @@ func setup_publishers(n int) ([]*zmq.Socket) {
 	pub_sockets := make([]*zmq.Socket, n)
 
 	for i := 0; i < n; i++ {
-		pub_sockets[i] = create_pub_socket(fmt.Sprintf("tcp://*:%d", 5550+i))
+		pub_sockets[i] = create_pub_socket(fmt.Sprintf("tcp://0.0.0.0:%d", 5550+i))
 	}
 
 	return pub_sockets
+}
+
+func get_files(path string) ([]string) {
+	files, _ := filepath.Glob(path)
+	return files
+}
+
+func run_handle(handle gopacket.PacketDataSource) {
+	var dec gopacket.Decoder
+	var ok bool
+
+	if dec, ok = gopacket.DecodersByLayerName[*decoder]; !ok {
+		log.Fatalln("No decoder named", *decoder)
+	}
+        source := gopacket.NewPacketSource(handle, dec)
+        for packet := range source.Packets() {
+                processed_packets += 1
+                packet_data := packet.Data()
+                if processed_packets % 100000 == 0 {
+                        fmt.Printf("Packets: %d\n", processed_packets)
+                }
+                publisher := publishers[processed_packets % *queues_num]
+                publisher.SendMessage(packet_data)
+        }
 }
 
 func main() {
 	defer util.Run()()
 	var handle *pcap.Handle
 	var err error
+
+	publishers = setup_publishers(*queues_num)
+
 	if *fname != "" {
-		if handle, err = pcap.OpenOffline(*fname); err != nil {
-			log.Fatal("PCAP OpenOffline error:", err)
+		files := get_files(*fname)
+
+		for i := 0; i < len(files); i++ {
+
+			fmt.Printf("pattern: %s num files: %d running: %s\n", *fname, len(files), files[i]);
+
+
+			if handle, err = pcap.OpenOffline(files[i]); err != nil {
+				log.Fatal("PCAP OpenOffline error:", err)
+			}	
+
+			run_handle(handle)
 		}
+
+
+
 	} else {
 		// This is a little complicated because we want to allow all possible options
 		// for creating the packet capture handle... instead of all this you can
@@ -86,19 +130,8 @@ func main() {
 				log.Fatal("BPF filter error:", err)
 			}
 		}
+
+		run_handle(handle)
 	}
 
-	publishers := setup_publishers(*queues_num)
-
-	ct := 0
-	source := gopacket.NewPacketSource(handle, handle.LinkType())
-	for packet := range source.Packets() {
-		ct += 1
-		packet_data := packet.Data()
-		if ct % 100000 == 0 {
-			fmt.Printf("Packets: %d\n", ct)
-		}
-		publisher := publishers[ct % *queues_num]
-		publisher.SendMessage(packet_data)
-	}
 }
